e91e836732aac1ecb08ddcf5053f756ca7c0cf6c
jd117 <juliodiogo@gmail.com>
2023-05-10 17:29:34 +0100

    SQL query, projection, GET games by lists

--- src/main/java/com/devsuperiorpt/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperiorpt/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperiorpt.dslist.dto;
 
 import com.devsuperiorpt.dslist.entities.Game;
+import com.devsuperiorpt.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -21,6 +22,14 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	
 	public Long getId() {
 		return id;
 	}
